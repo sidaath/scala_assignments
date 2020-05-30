a431064b929940c88dbb717a7597dc1916aeea77
sidaath <58439082+sidaath@users.noreply.github.com>
2020-05-30 21:40:03 +0530

    Add files via upload

---
+++ assignment3_question4.scala
@@ -0,0 +1,10 @@
+object checkNum extends App{
+
+	def isEven(n:Int):Boolean=n match{
+		case 0 =>true
+		case _ => isOdd(n-1)
+	}
+
+	def isOdd(n:Int):Boolean = !(isEven(n))
+	println(isEven(scala.io.StdIn.readInt()))
+}
