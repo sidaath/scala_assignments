e536dab01bb62e4c240f1ce5913d67f05c648e54
sidaath <58439082+sidaath@users.noreply.github.com>
2020-05-30 19:09:03 +0530

    Update book_price.scala

--- book_price.scala
+++ book_price.scala
@@ -1,4 +1,4 @@
-object final_price extend App {
+object final_price extends App {
 
 	def shipping_cost (x : Int) = (((x%50)*.75)+((x/50)*150));
 	def final_price (y: Int) = y*24.95*.6*shipping_cost(y); 
